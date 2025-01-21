69528c73a866cf0061dd97b60261c6ce5d817be2
Bryannn <bryannheinz@gmail.com>
2025-01-21 14:19:58 +0700

    typecasting in russt

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let character: char = 'A';
+
+    let integer = character as u8;
+
+    // From char to integer
+    println!("Character : {}", character);
+    println!("integer: {}", integer);
+}
