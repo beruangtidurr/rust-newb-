c486730f2859c6f208a0f31361a547377f0030f1
Bryannn <bryannheinz@gmail.com>
2025-01-21 14:21:08 +0700

    if statement in rust

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let x = 2;
+
+    if x % 2 == 0{
+        println!("its even");
+    } else {
+        println!("its odd");
+    }
+
+    let y = 3;
+
+    if y % 2 ==0{
+        println!("its even");
+    } else {
+        println!("its odd");
+    }
+}
